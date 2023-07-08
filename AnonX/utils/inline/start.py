f7bd3b849eff3820b455d3ee52340f3560c63a35
taitangamer <129022006+taitangamer@users.noreply.github.com>
2023-07-08 13:17:31 +0530

    Update start.py

--- AnonX/utils/inline/start.py
+++ AnonX/utils/inline/start.py
@@ -1,37 +1,25 @@
 from typing import Union
-import re
-import os
-from os import getenv
-
-from dotenv import load_dotenv
-
-from pyrogram import filters
-
 
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 import config
-load_dotenv()
-YOUR_GROUP = getenv("YOUR_GROUP", "https://t.me/timepassgroup01")
-YOUR_CHANNEL = getenv("YOUR_CHANNEL", "https://t.me/dangerous_fighter_clan_0")
-OWNER_USERNAME = getenv("OWNER_USERNAME", "https://t.me/taitangamerz")
-OWNER_USERNAME = getenv("OWNERR_USERNAME", "https://t.me/taitangamer")
+
 
 def start_pannel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
     buttons = [
         [
             InlineKeyboardButton(
-                text="✚ 𝐀ᴅᴅ 𝐌ᴇ 𝐌ᴏɪ 𝐋ᴜᴠ✚",
+                text="𝐀dd 𝐓αɪᴛαи 𝐌υ𝗌𝗂ç 𝐈𐓣 𝐆ʀσup",
                 url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
             )
         ],
         [
             InlineKeyboardButton(
-                text="🦋 𝐅ᴇᴀᴛᴜʀᴇ 🦋",
+                text="𝑯𝒆𝒍𝒑",
                 callback_data="settings_back_helper",
             ),
             InlineKeyboardButton(
-                text="⚙️ 𝐒ᴇᴛᴛɪɴɢ ⚙️", callback_data="settings_helper"
+                text="𝑺𝒆𝒕𝒕𝒊𝒏𝒈𝒔", callback_data="settings_helper"
             ),
         ],
      ]
@@ -42,30 +30,16 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
     buttons = [
         [
             InlineKeyboardButton(
-                text="✚ 𝐀ᴅᴅ 𝐌ᴇ 𝐌ᴏɪ 𝐋ᴜᴠ ✚",
+                text="𝐀dd 𝐓αɪᴛαи 𝐌υ𝗌𝗂ç 𝐈𐓣 𝐆ʀσup",
                 url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
             )
         ],
         [
             InlineKeyboardButton(
-                text="✨ 𝐌ᴀɴᴛᴀɪɴᴇʀ ✨", url=f"https://t.me/taitangamer",
+                text=" 𝑯𝒆𝒍𝒑 ", callback_data="settings_back_helper",
             ),
             InlineKeyboardButton(
-                text="🔎 𝐇ᴇʟᴘ 🔎", callback_data="settings_back_helper"
-            )
-        ],
-        [
-            InlineKeyboardButton(
-                text="💫 𝐒ᴜᴘᴘᴏʀᴛ 💫", url=f"https://t.me/DXDREAMBIG",
-            ),
-            InlineKeyboardButton(
-                text="🍁 𝐔ᴘᴅᴀᴛᴇ 🍁", url=f"https://t.me/taitanoffice",
-            )
-        ],
-        [
-            InlineKeyboardButton(
-                text="❄️ 𝐎ᴡɴᴇʀ ❄️",
-                url=f"https://t.me/Wine_killerxd",
+                text="𝐓αɪᴛαи 𝐌υ𝗌𝗂ç", url=f"https://music.apple.com/"
             )
         ],
      ]
